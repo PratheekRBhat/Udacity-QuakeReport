9d9b860d9b402d11d9fffe603ab99f08df86657e
0martel <pratheek.bht@gmail.com>
2020-03-02 20:50:33 +0530

    Adapter added

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,17 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+    private String mMagnitude, mLocation, mDate;
+
+    public Earthquake(String magnitude, String location, String date){
+        mMagnitude = magnitude;
+        mLocation = location;
+        mDate = date;
+    }
+
+    public String getMagnitude() { return mMagnitude;}
+
+    public String getLocation() { return mLocation;}
+
+    public String getDate() { return mDate;}
+}
---
+++ app/src/main/java/com/example/android/quakereport/Earthquake_Adapter.java
@@ -0,0 +1,40 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class Earthquake_Adapter extends ArrayAdapter<Earthquake> {
+    Earthquake_Adapter(Context context, List<Earthquake> earthquakes){
+        super(context, 0, earthquakes);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View ListItemView = convertView;
+        if(ListItemView == null){
+            ListItemView = LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item, parent, false);
+        }
+
+        Earthquake currentEarthquake = getItem(position);
+
+        TextView magnitudeView = (TextView) ListItemView.findViewById(R.id.quake_mag);
+        magnitudeView.setText(currentEarthquake.getMagnitude());
+
+        TextView locationView = (TextView) ListItemView.findViewById(R.id.location);
+        locationView.setText(currentEarthquake.getLocation());
+
+        TextView dateView = (TextView) ListItemView.findViewById(R.id.date);
+        dateView.setText(currentEarthquake.getDate());
+
+        return ListItemView;
+    }
+}
